2e100ea27ee5e766fad60d7f1279233ac194fdbf
Thibault Clérice <leponteineptique@gmail.com>
2018-01-15 15:20:17 +0100

    Chapitre 6

---
+++ cours-flask/exemple11/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask("Application")
+# On configure la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://gazetteer_user:password@localhost/gazetteer'
+# On initie l'extension
+db = SQLAlchemy(app)
+
+# On crée notre modèle
+class Place(db.Model):
+    place_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    place_nom = db.Column(db.Text)
+    place_description = db.Column(db.Text)
+    place_longitude = db.Column(db.Float)
+    place_latitude = db.Column(db.Float)
+    place_type = db.Column(db.String(45))
+
+
+@app.route("/")
+def accueil():
+    # On a bien sûr aussi modifié le template pour refléter le changement
+    lieux = Place.query.all()
+    return render_template("pages/accueil.html", nom="Gazetteer", lieux=lieux)
+
+
+@app.route("/place/<int:place_id>")
+def lieu(place_id):
+    # On a bien sûr aussi modifié le template pour refléter le changement
+    unique_lieu = Place.query.get(place_id)
+    return render_template("pages/place.html", nom="Gazetteer", lieu=unique_lieu)
+
+
+# Ce if permet de vérifier que ce fichier est celui qui est courrament exécuté. Cela permet par exemple d'éviter
+# de lancer une fonction quand on importe ce fichier depuis un autre fichier.
+# En python, lorsque l'on exécute un script avec la commande `python script.py`
+# Le fichier `script.py` a en __name__ la valeur __main__.
+if __name__ == "__main__":
+    app.run(debug=True)
