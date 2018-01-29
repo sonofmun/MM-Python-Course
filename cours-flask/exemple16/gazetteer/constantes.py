38bda56a3babbb22ee06c29993a852eb1e6e3102
Thibault Clérice <leponteineptique@gmail.com>
2018-01-29 18:00:56 +0100

    chapter 10 update

---
+++ cours-flask/exemple16/gazetteer/constantes.py
@@ -0,0 +1,7 @@
+
+LIEUX_PAR_PAGE = 2
+SEL = "Je suis un sel et vous devriez me changer !"
+
+if SEL == "Je suis un sel et vous devriez me changer !":
+    from warnings import warn
+    warn("Le sel par défaut n'a pas été changé, vous devriez le faire", Warning)
